c68a784141fdf2d12ab8261c23c28d726d805c06
Jonathan Thimesch <JRThimesch@gmail.com>
2019-04-10 23:28:32 -0500

    Add files via upload

---
+++ EmptyFolders.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+try:
+    for root, directories, files in os.walk("."):
+        for file in files:
+            if file.endswith('.mp3'):
+                if not os.path.isfile(os.path.join(".", file)):
+                    shutil.move(os.path.join(root, file), ".")
+                    print(file + " in " + root + " moved to ./")
+except IOError as err:
+    print("I/O error: {0}".format(err))
+except:
+    print("Unexpected error:", sys.exc_info()[0])
+    raise
