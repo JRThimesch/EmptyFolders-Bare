251152c705d1601c071e0ebfeeb98028c1f991e6
Jonathan Thimesch <JRThimesch@gmail.com>
2019-04-10 23:34:17 -0500

    Update EmptyFolders.py

--- EmptyFolders.py
+++ EmptyFolders.py
@@ -4,10 +4,9 @@ import shutil
 try:
     for root, directories, files in os.walk("."):
         for file in files:
-            if file.endswith('.mp3'):
-                if not os.path.isfile(os.path.join(".", file)):
-                    shutil.move(os.path.join(root, file), ".")
-                    print(file + " in " + root + " moved to ./")
+            if not os.path.isfile(os.path.join(".", file)):
+            	shutil.move(os.path.join(root, file), ".")
+                print(file + " in " + root + " moved to ./")
 except IOError as err:
     print("I/O error: {0}".format(err))
 except:
